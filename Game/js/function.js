40b55502b6dc8db306b7d263234685e92e744885
Guilherme-SM <guism2008@hotmail.com>
2018-08-06 21:04:26 -0300

    Movimentação aplicada, mais realista

--- Game/js/function.js
+++ Game/js/function.js
@@ -1,43 +1,102 @@
 alert("JS");
 // Funções do Jogo
+
 $(document).ready(function() {
 	
 
 	$("#energia").hide();
 	$("#explosao").hide();
 	$("#laser").hide();
+	$("#escudos").hide();
 });
 
 var TECLA = {
 	W:87,
 	A:65,
 	S:83,
-	D:68
+	D:68,
+	barra:32
 }
 
+var jogo = {W:87};
+jogo.press = [];
+
 $(function() {
+
+	jogo.timer = setInterval(loop,10);
+
+	$(document).keydown(function(e){
+	jogo.press[e.which] = true;
+	});
+
+	$(document).keyup(function(e){
+	jogo.press[e.which] = false;
+	});
+
+});
+
+function loop() {
+	movimento();
+}
+function movimento() {
+
+	if (jogo.press[TECLA.W]){
+
+		var topo = parseInt($("#player").css("top"));
+ 		$("#player").css("top",topo-2);
+
+ 		if (topo <= 0){
+ 			$("#player").css("top",topo+0);
+ 		}
+ 	}
+ 	if (jogo.press[TECLA.S]){
+
+		var desce = parseInt($("#player").css("top"));
+ 		$("#player").css("top",desce+2);
+	}
+	if (jogo.press[TECLA.D]){
+
+		var acc = parseInt($("#player").css("left"));
+ 		$("#player").css("left",acc+2);
+ 	}
+ 	if (jogo.press[TECLA.A]){
+
+		var bck = parseInt($("#player").css("left"));
+ 		$("#player").css("left",bck-2);
+ 	}
+}
+
+// $(function() {
 	
-	$(document).keydown(function(e)){
-		switch(e.which){
-			case TECLA.W:
+// 	$(document).keydown(function(e){
+// 		switch(e.which){
+			
+// 			case TECLA.W:
 
-				var topo = parseInt($("#player").css("top"));
-				$("#player").css("top",topo-10);break;
+// 				var topo = parseInt($("#player").css("top"));
+// 				$("#player").css("top",topo-2);break;
 
-			case TECLA.S:
+// 			case TECLA.S:
 
-				var desce = parseInt($("#player").css("top"));
-				$("#player").css("top",desce+10);break;
+// 				var desce = parseInt($("#player").css("top"));
+// 				$("#player").css("top",desce+10);break;
 
-			case TECLA.D:
+// 			case TECLA.D:
 
-				var acelera = parseInt($("#player").css("left"));
-				$("#player").css("top",acelera+10);break;
+// 				var acelera = parseInt($("#player").css("left"));
+// 				$("#player").css("left",acelera+10);break;
 
-			case TECLA.A:
+// 			case TECLA.A:
 
-				var recua = parseInt($("#player").css("left"));
-				$("#player").css("top",recua-10);break;
-		}
-	})
-})
+// 				var recua = parseInt($("#player").css("left"));
+// 				$("#player").css("left",recua-4);break;
+			
+// 			case TECLA.barra:
+
+// 				var spawn = parseInt($("#player").css("left")), parseInt($("#player"));
+// 				$("#player").css("left",spawn=395);
+// 				$("#player").css("top",spawn=395);break;
+
+// 		}
+// 	})
+// })
